8c9fc9f170768312909d9daa2663bb6ae39ff10e
Eric Wittenstein <eric.wittenstein@gmail.com>
2022-07-19 20:57:10 -0400

    forgot to do commits earlier, able to recover data, most of functionality tested

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,100 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// TODOx: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  else return '';
+}
 
-// TODO: Create a function that returns the license link
+// TODOx: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'None') {
+    return `\n* [License](#license)\n`;
+  }
+  else return '';
+}
 
-// TODO: Create a function that returns the license section of README
+// function to return a github repo placeholder if desired
+function insertRepoLinkReminder(wantReminder) {
+  if (wantReminder) {
+    return `\n## GitHub Repo
+
+    **_ THIS IS A PLACEHOLDER FOR A GITHUB REPO _**\n`;
+  }
+  else return '';
+}
+
+// function to return a screenshot/image placeholder if desired
+function insertImageReminder(wantReminder) {
+  if (wantReminder) {
+    return `\n## Project Demo
+
+    **_ THIS IS A PLACEHOLDER FOR AN IMAGE OR SCREEN RECORDING OF THE PROJECT _**\n`;
+  }
+  else return '';
+}
+
+// TODOx: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'None') {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+- This project is licensed under the ${license} license. -`;
+  }
+  return '';
+}
+
+// TODOx: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projTitle}
+${renderLicenseBadge(data.projLicns)}
 
+## Description
+
+${data.projDescr}
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+${renderLicenseLink(data.projLicns)}
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command in the console/command line:
+
+\`\`\`
+${data.projInstall}
+\`\`\`
+
+## Usage
+
+${data.projUsage}
+
+${renderLicenseSection(data.projLicns)}
+
+${insertRepoLinkReminder(data.projRepoLink)}
+
+${insertImageReminder(data.projDemoImage)}
+
+## Contributing
+
+${data.projContr}
+
+## Questions and Contact Info
+
+If you have any questions about the repo, please open an issue on GitHub or contact me directly at ${data.emailAddress}.
+
+You can find more of my work at [github.com/${data.githubUsername}/](https://github.com/${data.githubUsername}/).
 `;
 }
 
--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ## Having read the instructions, I have, to the best of my abilities, done the following:
 
-- TBD
+- used Inquirer NPM and NodeJS to create a custom README generator that runs in the console and outputs a .md file that can be used as a professional quality Github Repo README
 - added screenshots and links to this readme for the deployed application repo
 - pushed my files to GitHub and deployed them to a live site through GitHub
 - submitted the deployed application URL and GitHub repo to BCS for review and grading
